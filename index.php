78c02a06520b2a11eb2c28d89a2d42642805bf3e
ababkoff <31518073+ababkoff@users.noreply.github.com>
2017-09-05 14:47:06 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 </head>
 <body>
 <h1>Игра в города!</h1>
+<?php
 
+file_put_contents(__DIR__ . '/firstChar.txt', '');
+file_put_contents(__DIR__ . '/used.txt', '');
+?>
 
 <form action = "/city.php" method ="post">
     <input type = "string" name = "city">
